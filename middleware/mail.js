11d2e861f88995dfc4669aec30e7a85e7cda6d86
Partha4real <67099030+Partha4real@users.noreply.github.com>
2020-11-25 15:22:24 +0530

    Update mail.js

--- middleware/mail.js
+++ middleware/mail.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'partha.beis.16@acharya.ac.in',
-      pass: 'Shehates@1998'
+      user: '*****',  //enter your email
+      pass: '*****'  //enter password
     }
   });
   
